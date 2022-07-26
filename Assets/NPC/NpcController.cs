1cae9485695d07eff7d623f963de4debef939251
cookieopjax <vigor01695@gmail.com>
2022-07-26 19:26:47 +0800

    npc to homemap

--- Assets/NPC/NpcController.cs
+++ Assets/NPC/NpcController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NpcController : MonoBehaviour
 {
     [SerializeField] Vector2 _currentDistance; 
     [SerializeField] float _totalDistance = 10; 
     [SerializeField] float speed = 0.7f;
+
 
     void Update(){
 
@@ -20,5 +22,10 @@ public class NpcController : MonoBehaviour
             this.gameObject.transform.Translate(new Vector2(speed, 0) * Time.deltaTime);
 
 
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        SceneManager.LoadScene("HomeMap");
     }
 }
